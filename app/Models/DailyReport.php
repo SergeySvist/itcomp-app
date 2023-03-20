3451adf41bdcd10f2837d106a90a424b9c502ad4
SergeySvist <sergeysvist557@gmail.com>
2023-03-20 13:13:42 +0200

    add: files migration, model

--- app/Models/DailyReport.php
+++ app/Models/DailyReport.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DailyReports extends Model
+class DailyReport extends Model
 {
     use HasFactory;
 
